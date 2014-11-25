e39ad589c94e8e18fda98701fc2bdd4167003097
russell goldenberg <russellgoldenberg@gmail.com>
2014-11-25 13:12:12 -0500

    sortage bruh

--- barchart.js
+++ barchart.js
@@ -4,6 +4,7 @@
 	var _demo = 'Superette\t$8.00\nTasty Treat\t$5.00\nBig Fresh\t$9.00\nSeta\'s Cafe\t$7.50';
 	var _maxWidth = 80;
 	var _css = '\n.barchart-container {\n\tfont-family: Helvetica, Arial, sans-serif;\n\tfont-size: 15px;\n\tmargin: 1em 0;\n}\n.barchart-header {\n\tmargin-bottom: 0.5em;\n}\n.barchart-hed {\n\tfont-size: 1.4em;\n\tfont-weight: bold;\n}\n.barchart-subhed {\n\tfont-size: 1em;\n}\n.barchart-row {\n\t\n}\n.barchart-row-name {\n\tmargin: 0.1em 0 0.1em 0;\n\twidth: 100%;\n\tfont-size: 0.8em;\n}\n.barchart-row-bar {\n\theight: 1.2em;\n\tmargin: 0 0 .4em 0;\n\tbackground: #edece4;\n}\n.barchart-row-bar-inner {\n\tdisplay: inline-block;\n\theight: 100%;\n\tbackground: #bf6151;\n}\n.barchart-row-value {\n\tdisplay: inline-block;\n\tcolor: #888;\n\tvertical-align: top;\n\tline-height: 1.5;\n\tmargin-left: .5em;\n\tfont-size: .8em;\n\tfont-weight: 700;\n}\n.barchart-source-and-credit {\n\twidth: 100%;\n\toverflow: hidden;\n\tmargin-top: 1em;\n}\n.barchart-source-and-credit > div{\n\tmargin: 0;\n\ttext-align: right;\n}\n.barchart-credit {\n\tcolor: #999;\n\ttext-transform: uppercase;\n\tfloat: right;\n\tfont-size: 0.65em;\n}\n.barchart-source {\n\tfloat: left;\n\tfont-weight: bold;\n\tfont-size: 0.8em;\n}\n@media (max-width: 640px) {\n.barchart-credit {\n\tfloat: left;\n}\n}';
+	var _sortBy = 'default';
 
 	function init() {
 		bindEvents();
@@ -40,6 +41,15 @@
 		$('.preview').on('click', '.barchart-row', function() {
 			$(this).toggleClass('highlight');
 		});
+
+		$('.options-sort').on('click', function(e) {
+			e.preventDefault();
+			$('.options-sort').removeClass('current');
+			$(this).addClass('current');
+			_sortBy = $(this).attr('data-sort');
+			createChart();
+			return false;
+		})
 	}
 
 	function setPercentWidth() {
@@ -94,7 +104,7 @@
 		cb(err, result);
 	}
 
-	function createChart(input) {
+	function createChart() {
 		var $container = $('<div class="barchart-container"></div>');
 		var $header = $('<div class="barchart-header"></div>');
 		var $hed = $('<div contenteditable="true" class="barchart-hed">Hed</div>');
@@ -105,8 +115,11 @@
 		var $credit = $('<div contenteditable="true" class="barchart-credit">First Last / Globe Staff</div>');
 
 		var chartContent = '';
-		for(var i = 0; i < _result.data.length; i++) {
-			var datum = _result.data[i];
+
+		var sortedResults = sortResults();
+
+		for(var i = 0; i < sortedResults.length; i++) {
+			var datum = sortedResults[i];
 			chartContent += '<div class="barchart-row" id="' + datum.id + '">';
 			chartContent += '<div class="barchart-row-name">' + datum.name + '</div>';
 			chartContent += '<div class="barchart-row-bar"><span class="barchart-row-bar-inner" style="width: ' + datum.percent + '"></span>';
@@ -145,8 +158,6 @@
 			}
 		});
 
-		console.log(highlight);
-
 		var html = '';
 
 		html += '<style>' + _css + '\n</style>';
@@ -177,6 +188,31 @@
 		html += '\n</div>';
 
 		$('.output-code').val(html);
+	}
+
+	function sortResults() {
+		var dupe = _result.data.clone();
+		if(_sortBy === 'asc') {
+			dupe.sort(function(a,b) {
+				return a['value_number'] - b['value_number'];
+			});
+		} else if(_sortBy === 'desc') {
+			dupe.sort(function(a,b) {
+				return b['value_number'] - a['value_number'];
+			});
+		} else if(_sortBy === 'alph') {
+			dupe.sort(function(a,b) {
+				if(a['name'] < b['name']) {
+					return -1;
+				} else if(a['name'] > b['name']) {
+					return 1;
+				} else {
+					return 0;	
+				}
+			});
+		}
+
+		return dupe;
 	}
 
 	init();
