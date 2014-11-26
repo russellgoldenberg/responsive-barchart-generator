17f48ad65d647f5c6d55601252fcb05e446e7de1
russell goldenberg <russellgoldenberg@gmail.com>
2014-11-26 17:40:36 -0500

    readyish

--- barchart.js
+++ barchart.js
@@ -3,7 +3,7 @@
 	var _result;
 	var _demo = 'Superette\t$8.00\nTasty Treat\t$5.00\nBig Fresh\t$9.00\nSeta\'s Cafe\t$7.50';
 	var _maxWidth = 80;
-	var _css = '\n.barchart-container {\n\tfont-family: Helvetica, Arial, sans-serif;\n\tfont-size: 15px;\n\tmargin: 1em 0;\n\tcolor: #1a1a1a;\n}\n.barchart-header {\n\tmargin-bottom: 0.5em;\n}\n.barchart-hed {\n\tfont-size: 1.4em;\n\tfont-weight: bold;\n}\n.barchart-subhed {\n\tfont-size: 1em;\n}\n.barchart-row-name {\n\tmargin: 0.1em 0 0.1em 0;\n\twidth: 100%;\n\tfont-size: 0.9em;\n}\n.highlight .barchart-row-name {\n\tfont-weight: bold;\n}\n.barchart-row-bar {\n\theight: 1.2em;\n\tmargin: 0 0 .4em 0;\n}\n.barchart-row-bar-inner {\n\tdisplay: inline-block;\n\theight: 100%;\n\tbackground: #edece4;\n}\n.highlight .barchart-row-bar-inner {\n\tbackground: #bf6151;\n}\n.barchart-row-value {\n\tdisplay: inline-block;\n\tvertical-align: top;\n\tline-height: 1.5;\n\tmargin-left: .5em;\n\tfont-size: .8em;\n\tfont-weight: 700;\n}\n.barchart-source-and-credit {\n\tfont-family: Georgia,"Times New Roman",Times,serif;\n\twidth: 100%;\n\toverflow: hidden;\n\tmargin-top: 1em;\n}\n.barchart-source {\n\tmargin: 0;\n\tfloat: left;\n\tletter-spacing: 0.05em;\n\tfont-weight: bold;\n\tfont-size: 0.75em;\n}\n.barchart-credit {\n\tmargin: 0;\n\tcolor: #999;\n\ttext-transform: uppercase;\n\tfloat: right;\n\ttext-align: right;\n\tfont-size: 0.65em;\n}\n@media (max-width: 640px) {\n.barchart-source-and-credit > div {\n\twidth: 100%;\n\tdisplay: block;\n\tfloat: none;\n\ttext-align: right;\n}\n}';
+	var _css = '.barchart-container {\n\tfont-family: Helvetica, Arial, sans-serif;\n\tfont-size: 16px;\n\tmargin: 1em 0;\n\tcolor: #1a1a1a;\n}\n.barchart-header {\n\tmargin-bottom: 0.5em;\n}\n.barchart-hed {\n\tfont-size: 1.4em;\n\tfont-weight: bold;\n\tmargin-bottom: 0.2em;\n}\n.barchart-subhed {\n\tfont-size: 1em;\n}\n.barchart-row-name {\n\tmargin: 0.1em 0 0.1em 0;\n\twidth: 100%;\n\tfont-size: 0.9em;\n}\n.highlight .barchart-row-name {\n\tfont-weight: bold;\n}\n.barchart-row-bar {\n\theight: 1.2em;\n\tmargin: 0 0 .4em 0;\n}\n.barchart-row-bar-inner {\n\tdisplay: inline-block;\n\theight: 100%;\n\tbackground: #edece4;\n}\n.highlight .barchart-row-bar-inner {\n\tbackground: #bf6151;\n}\n.barchart-row-value {\n\tdisplay: inline-block;\n\tvertical-align: top;\n\tline-height: 1.5;\n\tmargin-left: .5em;\n\tfont-size: .8em;\n\tfont-weight: 700;\n}\n.barchart-source-and-credit {\n\tfont-family: Georgia,"Times New Roman",Times,serif;\n\twidth: 100%;\n\toverflow: hidden;\n\tmargin-top: 1em;\n}\n.barchart-source {\n\tmargin: 0;\n\tfloat: left;\n\tfont-weight: bold;\n\tfont-size: 0.75em;\n}\n.barchart-source .pre-colon {\n\ttext-transform: uppercase;\n}\n.barchart-credit {\n\tmargin: 0;\n\tcolor: #999;\n\ttext-transform: uppercase;\n\tletter-spacing: 0.05em;\n\tfloat: right;\n\ttext-align: right;\n\tfont-size: 0.65em;\n}\n@media (max-width: 640px) {\n.barchart-source-and-credit > div {\n\twidth: 100%;\n\tdisplay: block;\n\tfloat: none;\n\ttext-align: right;\n}\n}';
 	var _sortBy = 'default';
 	var _copy = {
 		hed: 'Hed',
@@ -29,6 +29,10 @@
 					_result = result;
 					setPercentWidth();
 					createChart();
+					var scrollTo = $('.output').offset().top - 10;
+					$('html, body').animate({
+						scrollTop: scrollTo
+					}, 250);
 				}
 			});
 			return false;
@@ -54,9 +58,18 @@
 			$('.options-sort').removeClass('current');
 			$(this).addClass('current');
 			_sortBy = $(this).attr('data-sort');
+			updateCopy();
 			createChart();
 			return false;
 		})
+	}
+
+	function updateCopy() {
+		_copy.hed = $('.barchart-hed').text();
+		_copy.subhed = $('.barchart-subhed').text();
+		_copy.credit = $('.barchart-credit').text();
+		_copy.sourcePre = $('.barchart-source .pre-colon').text();
+		_copy.sourcePost = $('.barchart-source .post-colon').text();
 	}
 
 	function setPercentWidth() {
@@ -155,11 +168,7 @@
 	}
 
 	function generateCode() {
-		_copy.hed = $('.barchart-hed').text();
-		_copy.subhed = $('.barchart-subhed').text();
-		_copy.credit = $('.barchart-credit').text();
-		_copy.sourcePre = $('.barchart-source .pre-colon').text();
-		_copy.sourcePost = $('.barchart-source .post-colon').text();
+		updateCopy();
 
 		var highlight = {};
 
@@ -175,8 +184,8 @@
 		html += '<style>' + _css + '\n</style>';
 		html += '\n<div class="barchart-container">';
 		html += '\n\t<div class="barchart-header">';
-		html += '\n\t\t<div class="barchart-hed">' + copy.hed + '</div>';
-		html += '\n\t\t<div class="barchart-subhed">' + copy.subhed + '</div>';
+		html += '\n\t\t<div class="barchart-hed">' + _copy.hed + '</div>';
+		html += '\n\t\t<div class="barchart-subhed">' + _copy.subhed + '</div>';
 		html += '\n\t</div>';
 		html += '\n\t<div class="barchart-content">';
 
@@ -194,8 +203,8 @@
 
 		html += '\n\t</div>';
 		html += '\n\t<div class="barchart-source-and-credit">';
-		html += '\n\t\t<div class="barchart-source"><span class="pre-colon">' + copy.sourcePre + '</span><span class="post-colon">' + copy.sourcePost + '</span></div>';
-		html += '\n\t\t<div class="barchart-credit">' + copy.credit + '</div>';
+		html += '\n\t\t<div class="barchart-source"><span class="pre-colon">' + _copy.sourcePre + '</span><span class="post-colon">' + _copy.sourcePost + '</span></div>';
+		html += '\n\t\t<div class="barchart-credit">' + _copy.credit + '</div>';
 		html += '\n\t</div>';
 		html += '\n</div>';
 
