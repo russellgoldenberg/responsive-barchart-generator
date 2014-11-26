64ea725b97fb6279b24afec7acfd9265169a08e3
russell goldenberg <russellgoldenberg@gmail.com>
2014-11-26 13:21:15 -0500

    save copy

--- barchart.js
+++ barchart.js
@@ -5,6 +5,13 @@
 	var _maxWidth = 80;
 	var _css = '\n.barchart-container {\n\tfont-family: Helvetica, Arial, sans-serif;\n\tfont-size: 15px;\n\tmargin: 1em 0;\n\tcolor: #1a1a1a;\n}\n.barchart-header {\n\tmargin-bottom: 0.5em;\n}\n.barchart-hed {\n\tfont-size: 1.4em;\n\tfont-weight: bold;\n}\n.barchart-subhed {\n\tfont-size: 1em;\n}\n.barchart-row-name {\n\tmargin: 0.1em 0 0.1em 0;\n\twidth: 100%;\n\tfont-size: 0.9em;\n}\n.highlight .barchart-row-name {\n\tfont-weight: bold;\n}\n.barchart-row-bar {\n\theight: 1.2em;\n\tmargin: 0 0 .4em 0;\n}\n.barchart-row-bar-inner {\n\tdisplay: inline-block;\n\theight: 100%;\n\tbackground: #edece4;\n}\n.highlight .barchart-row-bar-inner {\n\tbackground: #bf6151;\n}\n.barchart-row-value {\n\tdisplay: inline-block;\n\tvertical-align: top;\n\tline-height: 1.5;\n\tmargin-left: .5em;\n\tfont-size: .8em;\n\tfont-weight: 700;\n}\n.barchart-source-and-credit {\n\tfont-family: Georgia,"Times New Roman",Times,serif;\n\twidth: 100%;\n\toverflow: hidden;\n\tmargin-top: 1em;\n}\n.barchart-source {\n\tmargin: 0;\n\tfloat: left;\n\tletter-spacing: 0.05em;\n\tfont-weight: bold;\n\tfont-size: 0.75em;\n}\n.barchart-credit {\n\tmargin: 0;\n\tcolor: #999;\n\ttext-transform: uppercase;\n\tfloat: right;\n\ttext-align: right;\n\tfont-size: 0.65em;\n}\n@media (max-width: 640px) {\n.barchart-source-and-credit > div {\n\twidth: 100%;\n\tdisplay: block;\n\tfloat: none;\n\ttext-align: right;\n}\n}';
 	var _sortBy = 'default';
+	var _copy = {
+		hed: 'Hed',
+		subhed: 'Subhed goes here.',
+		credit: 'Full Name / Globe Staff',
+		sourcePre: 'DATA',
+		sourcePost: 'Sources'
+	};
 
 	function init() {
 		bindEvents();
@@ -113,11 +120,11 @@
 	function createChart() {
 		var $container = $('<div class="barchart-container"></div>');
 		var $header = $('<div class="barchart-header"></div>');
-		var $hed = $('<div contenteditable="true" class="barchart-hed">Hed</div>');
-		var $subhed = $('<div contenteditable="true" class="barchart-subhed">A subhed goes here</div>');
+		var $hed = $('<div contenteditable="true" class="barchart-hed">' + _copy.hed + '</div>');
+		var $subhed = $('<div contenteditable="true" class="barchart-subhed">' + _copy.subhed + '</div>');
 		var $chart = $('<div class="barchart-content"></div>');
-		var $sourceCredit = $('<div class="barchart-source-and-credit"></div>');
-		var $source = $('<div class="barchart-source"><span contenteditable="true" class="pre-colon">Data:</span> <span contenteditable="true" class="post-colon">My Source</span></div>');
+		var $sourceCredit = $('<div class="barchart-source-and-credit">' + _copy.credit + '</div>');
+		var $source = $('<div class="barchart-source"><span contenteditable="true" class="pre-colon">' + _copy.sourcePre + '</span>: <span contenteditable="true" class="post-colon">' + _copy.sourcePost + '</span></div>');
 		var $credit = $('<div contenteditable="true" class="barchart-credit">First Last / Globe Staff</div>');
 
 		var chartContent = '';
@@ -148,13 +155,11 @@
 	}
 
 	function generateCode() {
-		var copy = {
-			hed: $('.barchart-hed').text(),
-			subhed: $('.barchart-subhed').text(),
-			credit: $('.barchart-credit').text(),
-			sourcePre: $('.barchart-source .pre-colon').text(),
-			sourcePost: $('.barchart-source .post-colon').text()
-		};
+		_copy.hed = $('.barchart-hed').text();
+		_copy.subhed = $('.barchart-subhed').text();
+		_copy.credit = $('.barchart-credit').text();
+		_copy.sourcePre = $('.barchart-source .pre-colon').text();
+		_copy.sourcePost = $('.barchart-source .post-colon').text(;
 
 		var highlight = {};
 
