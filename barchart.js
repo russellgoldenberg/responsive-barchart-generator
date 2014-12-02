8d45517a3b2175fbf6ce508463570550d24a904d
russell goldenberg <russellgoldenberg@gmail.com>
2014-12-02 13:02:50 -0500

    Many updates, globe style

--- barchart.js
+++ barchart.js
@@ -3,12 +3,11 @@
 	var _result;
 	var _demo = 'Superette\t$8.00\nTasty Treat\t$5.00\nBig Fresh\t$9.00\nSeta\'s Cafe\t$7.50';
 	var _maxWidth = 80;
-	var _css = '.responsive-barchart-container {\n\tfont-family: Helvetica, Arial, sans-serif;\n\tfont-size: 16px;\n\tmargin: 1em 0;\n\tcolor: #1a1a1a;\n}\n.responsive-barchart-header {\n\tmargin-bottom: 0.5em;\n}\n.responsive-barchart-hed {\n\tfont-size: 1.4em;\n\tfont-weight: bold;\n\tmargin-bottom: 0.2em;\n}\n.responsive-barchart-subhed {\n\tfont-size: 1em;\n}\n.responsive-barchart-row-name {\n\tmargin: 0.1em 0 0.1em 0;\n\twidth: 100%;\n\tfont-size: 0.9em;\n}\n.highlight .responsive-barchart-row-name {\n\tfont-weight: bold;\n}\n.responsive-barchart-row-bar {\n\theight: 1.2em;\n\tmargin: 0 0 .4em 0;\n}\n.responsive-barchart-row-bar-inner {\n\tdisplay: inline-block;\n\theight: 100%;\n\tbackground: #edece4;\n}\n.highlight .responsive-barchart-row-bar-inner {\n\tbackground: #bf6151;\n}\n.responsive-barchart-row-value {\n\tdisplay: inline-block;\n\tvertical-align: top;\n\tline-height: 1.5;\n\tmargin-left: .5em;\n\tfont-size: .8em;\n\tfont-weight: 700;\n}\n.responsive-barchart-source-and-credit {\n\tfont-family: Georgia,"Times New Roman",Times,serif;\n\twidth: 100%;\n\toverflow: hidden;\n\tmargin-top: 1em;\n}\n.responsive-barchart-source {\n\tmargin: 0;\n\tfloat: left;\n\tfont-weight: bold;\n\tfont-size: 0.75em;\n}\n.responsive-barchart-source .pre-colon {\n\ttext-transform: uppercase;\n}\n.responsive-barchart-credit {\n\tmargin: 0;\n\tcolor: #999;\n\ttext-transform: uppercase;\n\tletter-spacing: 0.05em;\n\tfloat: right;\n\ttext-align: right;\n\tfont-size: 0.65em;\n}\n@media (max-width: 640px) {\n.responsive-barchart-source-and-credit > div {\n\twidth: 100%;\n\tdisplay: block;\n\tfloat: none;\n\ttext-align: right;\n}\n}';
 	var _sortBy = 'default';
 	var _copy = {
 		hed: 'Hed',
 		subhed: 'Subhed goes here.',
-		credit: 'First Last / Globe Staff',
+		credit: 'Globe Staff',
 		sourcePre: 'DATA',
 		sourcePost: 'Sources'
 	};
@@ -49,7 +48,7 @@
 			generateCode();
 		});
 
-		$('.preview').on('click', '.responsive-barchart-row', function() {
+		$('.preview').on('click', '.rg-barchart-row', function() {
 			$(this).toggleClass('highlight');
 		});
 
@@ -65,11 +64,11 @@
 	}
 
 	function updateCopy() {
-		_copy.hed = $('.responsive-graphic-hed').text();
-		_copy.subhed = $('.responsive-graphic-subhed').text();
-		_copy.credit = $('.responsive-graphic-credit').text();
-		_copy.sourcePre = $('.responsive-graphic-source .pre-colon').text();
-		_copy.sourcePost = $('.responsive-graphic-source .post-colon').text();
+		_copy.hed = $('.rg-hed').text();
+		_copy.subhed = $('.rg-subhed').text();
+		_copy.credit = $('.rg-credit').text();
+		_copy.sourcePre = $('.rg-source .pre-colon').text();
+		_copy.sourcePost = $('.rg-source .post-colon').text();
 	}
 
 	function setPercentWidth() {
@@ -131,14 +130,14 @@
 	}
 
 	function createChart() {
-		var $container = $('<div class="responsive-graphic-container"></div>');
-		var $header = $('<div class="responsive-graphic-header"></div>');
-		var $hed = $('<div contenteditable="true" class="responsive-graphic-hed">' + _copy.hed + '</div>');
-		var $subhed = $('<div contenteditable="true" class="responsive-graphic-subhed">' + _copy.subhed + '</div>');
-		var $content = $('<div class="responsive-graphic-content rg-barchart"></div>');
-		var $sourceCredit = $('<div class="responsive-graphic-source-and-credit"></div>');
-		var $source = $('<div class="responsive-graphic-source"><span contenteditable="true" class="pre-colon">' + _copy.sourcePre + '</span>: <span contenteditable="true" class="post-colon">' + _copy.sourcePost + '</span></div>');
-		var $credit = $('<div contenteditable="true" class="responsive-graphic-credit">' + _copy.credit + '</div>');
+		var $container = $('<div class="rg-container"></div>');
+		var $header = $('<div class="rg-header"></div>');
+		var $hed = $('<div contenteditable="true" class="rg-hed">' + _copy.hed + '</div>');
+		var $subhed = $('<div contenteditable="true" class="rg-subhed">' + _copy.subhed + '</div>');
+		var $content = $('<div class="rg-content rg-barchart"></div>');
+		var $sourceCredit = $('<div class="rg-source-and-credit"></div>');
+		var $source = $('<div contenteditable="true" class="rg-source"><span class="pre-colon">' + _copy.sourcePre + '</span>: <span class="post-colon">' + _copy.sourcePost + '</span></div>');
+		var $credit = $('<div contenteditable="true" class="rg-credit">' + _copy.credit + '</div>');
 
 		var chartContent = '';
 
@@ -146,10 +145,10 @@
 
 		for(var i = 0; i < sortedResults.length; i++) {
 			var datum = sortedResults[i];
-			chartContent += '<div class="responsive-barchart-row" id="' + datum.id + '">';
-			chartContent += '<div class="responsive-barchart-row-name">' + datum.name + '</div>';
-			chartContent += '<div class="responsive-barchart-row-bar"><span class="responsive-barchart-row-bar-inner" style="width: ' + datum.percent + '"></span>';
-			chartContent += '<span class="responsive-barchart-row-value">' + datum.value + '</span>';
+			chartContent += '<div class="rg-barchart-row" id="' + datum.id + '">';
+			chartContent += '<div class="rg-barchart-row-name">' + datum.name + '</div>';
+			chartContent += '<div class="rg-barchart-row-bar"><span class="rg-barchart-row-bar-inner" style="width: ' + datum.percent + '"></span>';
+			chartContent += '<span class="rg-barchart-row-value">' + datum.value + '</span>';
 			chartContent += '</div></div>';
 		}
 
@@ -172,44 +171,46 @@
 
 		var highlight = {};
 
-		$('.responsive-barchart-row').each(function() {
+		$('.rg-barchart-row').each(function() {
 			if($(this).hasClass('highlight')) {
 				var id = $(this).attr('id');
 				highlight[id] = true;
 			}
 		});
 
+		var css = '\n/*styles for graphic info (hed, subhed, source, credit)*/\n.rg-container {\n\tfont-family: Helvetica, Arial, sans-serif;\n\tfont-size: 16px;\n\tmargin: 1em 0;\n\tcolor: #1a1a1a;\n}\n.rg-header {\n\tmargin-bottom: 1em;\n}\n.rg-hed {\n\tfont-size: 1.6em;\n\tfont-weight: bold;\n\tmargin-bottom: 0.25em;\n}\n.rg-subhed {\n\tfont-size: 1em;\n\tline-height: 1.4em;\n}\n.rg-source-and-credit {\n\tfont-family: Georgia,"Times New Roman",Times,serif;\n\twidth: 100%;\n\toverflow: hidden;\n\tmargin-top: 1em;\n}\n.rg-source {\n\tmargin: 0;\n\tfloat: left;\n\tfont-weight: bold;\n\tfont-size: 0.75em;\n\tline-height: 1.5em;\n}\n.rg-source .pre-colon {\n\ttext-transform: uppercase;\n}\n.rg-credit {\n\tmargin: 0;\n\tcolor: #999;\n\ttext-transform: uppercase;\n\tletter-spacing: 0.05em;\n\tfloat: right;\n\ttext-align: right;\n\tfont-size: 0.65em;\n\tline-height: 1.5em;\n}\n@media (max-width: 640px) {\n.rg-source-and-credit > div {\n\twidth: 100%;\n\tdisplay: block;\n\tfloat: none;\n\ttext-align: right;\n}\n}\n/*styles for graphic*/\n.rg-barchart-row {\n\tmargin-bottom: 1em;\n}\n\n.rg-barchart-row-name {\n\tmargin: 0.1em 0 0.1em 0;\n\twidth: 100%;\n\tfont-size: 0.85em;\n\tcolor: #222;\n}\n.highlight .rg-barchart-row-name {\n\tfont-weight: bold;\n}\n.rg-barchart-row-bar {\n\theight: 1.2em;\n}\n.rg-barchart-row-bar-inner {\n\tdisplay: inline-block;\n\theight: 100%;\n\tbackground: #edece4;\n}\n.highlight .rg-barchart-row-bar-inner {\n\tbackground: #bf6151;\n}\n.rg-barchart-row-value {\n\tdisplay: inline-block;\n\tvertical-align: top;\n\tline-height: 1.5;\n\tmargin-left: .5em;\n\tfont-size: .8em;\n\tfont-weight: 700;\n}';
+
 		var html = '';
 
-		html += '<style>' + _css + '\n</style>';
-		html += '\n<div class="responsive-graphic-container">';
-		html += '\n\t<div class="responsive-graphic-header">';
+		html += '<style>' + css + '\n</style>';
+		html += '\n<div class="rg-container">';
+		html += '\n\t<div class="rg-header">';
 
 		if(_copy.hed.length > 0 ) {
-			html += '\n\t\t<div class="responsive-graphic-hed">' + _copy.hed + '</div>';	
+			html += '\n\t\t<div class="rg-hed">' + _copy.hed + '</div>';	
 		}
 		if(_copy.subhed.length > 0 ) {
-			html += '\n\t\t<div class="responsive-graphic-subhed">' + _copy.subhed + '</div>';	
+			html += '\n\t\t<div class="rg-subhed">' + _copy.subhed + '</div>';	
 		}
 		html += '\n\t</div>';
-		html += '\n\t<div class="responsive-graphic-content">';
+		html += '\n\t<div class="rg-content">';
 
 		for(var i = 0; i < _result.data.length; i++) {
 			var datum = _result.data[i];
 			hiClass = highlight[datum.id] ? ' highlight' : '';
-			html += '\n\t\t<div class="responsive-barchart-row' + hiClass + '" id="' + datum.id + '">';	
-			html += '\n\t\t\t<div class="responsive-barchart-row-name">' + datum.name + '</div>';
-			html += '\n\t\t\t<div class="responsive-barchart-row-bar">';
-			html += '\n\t\t\t\t<span class="responsive-barchart-row-bar-inner" style="width: ' + datum.percent + '"></span>';
-			html += '\n\t\t\t\t<span class="responsive-barchart-row-value">' + datum.value + '</span>';
+			html += '\n\t\t<div class="rg-barchart-row' + hiClass + '" id="' + datum.id + '">';	
+			html += '\n\t\t\t<div class="rg-barchart-row-name">' + datum.name + '</div>';
+			html += '\n\t\t\t<div class="rg-barchart-row-bar">';
+			html += '\n\t\t\t\t<span class="rg-barchart-row-bar-inner" style="width: ' + datum.percent + '"></span>';
+			html += '\n\t\t\t\t<span class="rg-barchart-row-value">' + datum.value + '</span>';
 			html += '\n\t\t\t</div>'
 			html += '\n\t\t</div>';
 		}
 
 		html += '\n\t</div>';
-		html += '\n\t<div class="responsive-graphic-source-and-credit">';
-		html += '\n\t\t<div class="responsive-graphic-source"><span class="pre-colon">' + _copy.sourcePre + '</span>: <span class="post-colon">' + _copy.sourcePost + '</span></div>';
-		html += '\n\t\t<div class="responsive-graphic-credit">' + _copy.credit + '</div>';
+		html += '\n\t<div class="rg-source-and-credit">';
+		html += '\n\t\t<div class="rg-source"><span class="pre-colon">' + _copy.sourcePre + '</span>: <span class="post-colon">' + _copy.sourcePost + '</span></div>';
+		html += '\n\t\t<div class="rg-credit">' + _copy.credit + '</div>';
 		html += '\n\t</div>';
 		html += '\n</div>';
 
